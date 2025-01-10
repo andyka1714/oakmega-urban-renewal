a8ef5d836c667e68ed04b074c068376fd15e5bf1
Andy <andyka1714@gmail.com>
2025-01-10 23:34:07 +0800

    feat: update location

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const Home = () => {
-  return <MapContainer center={[25.032969, 121.565418]} zoom={16} style={{ height: '600px', width: '100%' }}>
+  return <MapContainer center={[25.061691,121.4847221]} zoom={16} style={{ height: '600px', width: '100%' }}>
   <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 </MapContainer>;
 }
