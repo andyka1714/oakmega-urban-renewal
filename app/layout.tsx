9d35ef5dadcdf13f50e476c1ec6ba7b193661146
Andy <andyka1714@gmail.com>
2025-01-14 21:46:17 +0800

    chore: move ReduxProvider to common folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ReduxProvider from './ReduxProvider';
+import ReduxProvider from './common/ReduxProvider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
